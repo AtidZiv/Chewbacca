2b1d6a0049286fb442796650b6b9e2e8954314cb
AtidZiv <30617236+AtidZiv@users.noreply.github.com>
2017-08-05 23:06:57 +0300

    changed list view to dynamic - doesn't work, need to fix

--- MyGuide/app/src/main/java/com/chewie/myguide/CategoriesActivity.java
+++ MyGuide/app/src/main/java/com/chewie/myguide/CategoriesActivity.java
@@ -4,18 +4,49 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CategoriesActivity extends AppCompatActivity {
+    ListView listView;
+    SimpleCursorAdapter mAdapter;
+    ArrayList<String> chooseAction;
+    View row;
+    LayoutInflater inflater = getLayoutInflater();
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
-        MediaPlayer mediaPlayer = MediaPlayer.create(CategoriesActivity.this, R.raw.select_category);
-        mediaPlayer.start();
+        ArrayAdapter<String> adapter;
+       // String [] chooseAction = {"הוספת יישומונים(ויג'טים)", "אנשי קשר", "שימוש בחנות אפליקציות"};
+        listView = (ListView)findViewById(R.id.list_view);
+        chooseAction = new ArrayList<String>();
+        chooseAction.add("הוספת יישומונים(ויג'טים)");
+        chooseAction.add("אנשי קשר");
+        chooseAction.add("שימוש בחנות אפליקציות");
+        //adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, chooseAction);
+        //listView.setAdapter(adapter);
+        listView.setAdapter(new ArrayAdapter<String>(this, 0, chooseAction));
+
+        //MediaPlayer mediaPlayer = MediaPlayer.create(CategoriesActivity.this, R.raw.select_category);
+        //mediaPlayer.start();
     }
-    public void onSubCatrgories(View view)
+    public View getView(int position, View convertView, ViewGroup parent){
+            row = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            tv = (TextView) row.findViewById(android.R.id.text1);
+            tv.setText(chooseAction.get(position));
+            return row;
+};
+    public void onSubCatergories(View view)
     {
         Intent intent = new Intent(this, SubCategoriesActivity.class);
         startActivity(intent);
